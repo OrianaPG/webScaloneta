49e1d2160b2e284ac07f9fc30991941cf4ae27c9
Oriana <68253167+orianapg@users.noreply.github.com>
2022-12-22 10:49:35 -0300

    Borrar código extra

--- js/jugadores.js
+++ js/jugadores.js
@@ -15,15 +15,13 @@ form.addEventListener("submit", (event) => {
     window.location.href = "./index.html";
 });
 
-
 // Pedir datos a la API
 
-//? let respuesta = fetch("https://cors-anywhere.herokuapp.com/https://scaloneta-api.vercel.app/jugadores")
-
+//! Si uso JSON
 let respuesta = fetch("./js/jugadores.json")
     .then(respuesta => respuesta.json())
     .then(json => {
-        let jugadores = json.map(json => 
+        let jugadores = json.map(json =>
             `<div class="col-12 col-md-4 p-2 float-start">
                 <div class="card">
                     <div class="img-wrapper">
@@ -46,3 +44,31 @@ let respuesta = fetch("./js/jugadores.json")
         section.innerHTML = jugadores.join(" ");
     })
     .catch(error => console.log("Error: ", error));
+
+//! Si uso API
+// let respuesta = fetch("https://cors-anywhere.herokuapp.com/https://scaloneta-api.vercel.app/jugadores")
+//     .then(respuesta => respuesta.json())
+//     .then(data => {
+//         let jugadores = data.map(jugadores =>
+//             `<div class="col-12 col-md-4 p-2 float-start">
+//                 <div class="card">
+//                     <div class="img-wrapper">
+//                         <span id="numeroCamiseta">${jugadores.dorsal}</span>
+//                         <img src="https://julioavantt.github.io/guayerd-project-images/img/${json.dorsal}.jpg" class="card-img-top" alt="${json.name}">
+//                     </div>
+//                     <div class="card-body">
+//                         <h5 class="card-title">${jugadores.name}</h5>
+//                         <p class="card-text">Edad: ${jugadores.edad} años</p>
+//                         <p class="card-text">Posición: ${jugadores.posicion}</p>
+//                         <p class="card-text">Peso: ${jugadores["stats-fisico"].peso} kg</p>
+//                         <p class="card-text">Altura: ${jugadores["stats-fisico"].altura} m</p>
+//                         <p class="card-text">Equipo actual: ${jugadores["equipo-actual"]}</p>
+//                         <p class="card-text">Fecha de nacimiento: ${jugadores["fecha-nacimiento"]}</p>
+//                     </div>
+//                 </div>
+//             </div>`
+//         );
+//         let section = document.querySelector("section");
+//         section.innerHTML = jugadores.join(" ");
+//     })
+//     .catch(error => console.log("Error: ", error));
